f797985524e346c6dd45915cffc239716b5a91e2
AyoAdeyemo <165864366+AyoAdeyemo@users.noreply.github.com>
2025-03-09 19:41:53 +0100

    Painting Project Cost.py

--- Painting Project Cost.py
+++ Painting Project Cost.py
@@ -7,10 +7,10 @@ Created on Sun Mar  9 18:56:59 2025
 
 colors = int(input("Enter the number of colors needed:"))
 
-def cost_painting_project(num_colors, tax):
-    total_color_cost = 5 * num_colors
-    final_cost_without_tax = total_color_cost + tax
-    final_cost_with_tax = 1.1 * final_cost_without_tax
+def cost_painting_project(num_colors, other_cost):
+    total_color_cost = 5 * num_colors                                  #num_colors: number colors needed for painting project
+    final_cost_without_tax = total_color_cost + other_cost             #other cost includes the cost of canvas and brushes
+    final_cost_with_tax = 1.1 * final_cost_without_tax                 #10% tax on total price
     
     return final_cost_with_tax
 
