dee1753196ee30f3d62a62ca4fc54f118cf49404
AyoAdeyemo <165864366+AyoAdeyemo@users.noreply.github.com>
2025-06-28 23:12:37 +0200

    Add files via upload

---
+++ Construct Some Snakes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 28 20:48:39 2025
+
+@author: Ayobami Adeyemo
+"""
+
+how_many_snakes = 3
+snake_string = """
+Welcome to Python3!
+
+======
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
